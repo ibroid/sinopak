c648185d9a9d3111652b4680163b9d09a8cf1615
ibroid <maulana858@gmail.com>
2023-08-07 18:24:00 +0700

    update notifikasi spv

--- services/Spv/Notifikasi.js
+++ services/Spv/Notifikasi.js
@@ -2,14 +2,24 @@ import prismaSipp from "../../db/sipp.js"
 import prismaSinopak from "../../db/sinopak.js"
 import { sendMessage } from "../../utils/send_wa.js"
 import { dateIndo } from "../../utils/date_helper.js";
+import { logSave } from "../../utils/log_save.js";
+import { rupiah } from "../../utils/konkurensi.js";
+import angkaTerbilangJs from "@develoka/angka-terbilang-js";
+import moment from "moment";
 
 export async function sendNotifTest(request) {
     const pesan = await notifText(request.notifikasi_id);
     if (pesan == null) {
         throw new Error('Terjadi kesalahan saat mengambil pesan notif')
     }
-    await sendMessage(request.number, pesan)
-    saveLog(pesan, request.number, request.notifikasi_id)
+
+    await sendMessage(request.number, pesan.text)
+    await logSave({
+        id: pesan.jenis_notifikasi_id,
+        pesan: pesan.text,
+        number: request.number,
+        tujuan: pesan.tujuan
+    })
 }
 
 export async function notifText(notifikasi_id) {
@@ -19,18 +29,26 @@ export async function notifText(notifikasi_id) {
             select: {
                 nomor_perkara: true,
                 para_pihak: true,
-                perkara_jadwal_sidang: {
+                perkara_putusan: {
                     select: {
-                        agenda: true,
-                        tanggal_sidang: true,
-                        urutan: true
-                    },
-                    where: {
-                        urutan: 1
+                        amar_putusan: true,
+                        tanggal_putusan: true,
+                        putusan_verstek: true
                     }
                 },
-                perkara_pihak1: true,
-                perkara_pihak2: true,
+                perkara_biaya: true,
+                perkara_pihak1: {
+                    select: {
+                        nama: true,
+                        alamat: true
+                    }
+                },
+                perkara_pihak2: {
+                    select: {
+                        nama: true,
+                        alamat: true
+                    }
+                },
             },
             where: {
                 nomor_perkara: "123/Pdt.G/2023/" + process.env.PERKARA_KODE,
@@ -43,27 +61,33 @@ export async function notifText(notifikasi_id) {
         })
     ])
 
-    let dataPihakSatu = '';
-    let dataPihakDua = '';
+    let saldoMasuk = 0;
+    let saldoKeluar = 0;
 
-    data[0].perkara_pihak1.forEach(pihak => {
-        dataPihakSatu += `Nama : ${pihak.nama}\nAlamat : ${pihak.alamat}`
-    })
+    const putusDate = moment(data[0].perkara_putusan.tanggal_putusan, 'YYYY-MM-DD');
+    if (putusDate.isSame(Date.now(), 'day')) {
+        saldoKeluar += 20000;
+    }
 
-    data[0].perkara_pihak2.forEach(pihak => {
-        dataPihakDua += `Nama : ${pihak.nama}\nAlamat : ${pihak.alamat}`
-    })
+    data[0].perkara_biaya.forEach(row => {
+        console.log(row.jenis_transaksi + " - " + row.jumlah)
+        if (row.jenis_transaksi == 1) {
+            saldoMasuk += parseFloat(String(row.jumlah).replace('.00', ''));
+        } else {
+            saldoKeluar += parseFloat(String(row.jumlah).replace('.00', ''));
+        }
+
+    });
 
     const pesan = data[1].pesan
         .replace('{nomor_perkara}', data[0].nomor_perkara)
-        .replace('{tanggal_sidang}', dateIndo(data[0].perkara_jadwal_sidang[0].tanggal_sidang))
-        .replace('{agenda_sidang}', data[0].perkara_jadwal_sidang[0].agenda)
-        .replace('{para_pihak}', data[0].para_pihak)
-        .replace('{data_pihak_satu}', dataPihakSatu)
-        .replace('{data_pihak_dua}', dataPihakDua)
+        .replace('{tanggal_putus}', dateIndo(data[0].perkara_putusan.tanggal_putusan))
+        .replace('{nama_pihak}', data[0].perkara_pihak2[0].nama)
+        .replace('{alamat_pihak}', data[0].perkara_pihak2[0].alamat)
+        .replace('{sisa_panjar}', rupiah(String(saldoMasuk - saldoKeluar)))
+        .replace('{terbilang_sisa_panjar}', angkaTerbilangJs(saldoMasuk - saldoKeluar));
 
     return pesan;
-
 
 }
 
